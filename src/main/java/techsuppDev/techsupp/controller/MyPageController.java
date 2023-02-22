6eade924d6f2c09c7e715d6d066e8cdd9f027dba
moonbeom <tjansqja789@naver.com>
2023-02-22 14:00:02 +0900

    회원정보수정 만드는 - Ing

--- src/main/java/techsuppDev/techsupp/controller/MyPageController.java
+++ src/main/java/techsuppDev/techsupp/controller/MyPageController.java
@@ -1,5 +1,20 @@
 package techsuppDev.techsupp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class MyPageController {
+
+    @GetMapping("/edituser") // modelandview 모델을 뷰에 던져준다는 개념임.
+    public ModelAndView editUser() {
+        ModelAndView mav = new ModelAndView("mypage/editUser");
+        return mav;
+    }
+
+
+
 
 }
